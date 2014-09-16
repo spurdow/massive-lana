e4fa7189d25f6e05ff3201e9d4b9df27b5af6a9b
David <montecillodavid@gmail.com>
2014-09-16 21:10:41 +0800

    updaed

---
+++ crouton/build/generated/source/buildConfig/release/de/keyboardsurfer/mobile/app/android/widget/crouton/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package de.keyboardsurfer.mobile.app.android.widget.crouton;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String PACKAGE_NAME = "de.keyboardsurfer.mobile.app.android.widget.crouton";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
