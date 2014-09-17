8da5d5f78bec2a880525f97bf86a01a2a3c17464
David <montecillodavid@gmail.com>
2014-09-18 01:58:21 +0800

    updaed

---
+++ gtpr/build/generated/source/buildConfig/dev/gtg/virus/gtpr/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package gtg.virus.gtpr;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "gtg.virus.gtpr";
+  public static final String BUILD_TYPE = "dev";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0-unsigned";
+}
