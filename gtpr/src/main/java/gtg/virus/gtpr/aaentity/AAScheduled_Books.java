9c31afea4d999440bb0c8f5110f4b49c06e189de
David Montecillo <montecillodavid@gmail.com>
2015-02-23 02:10:23 +0800

    remove mp3 in booktask

--- gtpr/src/main/java/gtg/virus/gtpr/aaentity/AAScheduled_Books.java
+++ gtpr/src/main/java/gtg/virus/gtpr/aaentity/AAScheduled_Books.java
@@ -2,6 +2,7 @@ package gtg.virus.gtpr.aaentity;
 
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
 import com.activeandroid.query.Select;
 
 import java.util.Date;
@@ -10,7 +11,7 @@ import java.util.List;
 /**
  * Created by DavidLuvelleJoseph on 2/14/2015.
  */
-/*@Table(name = "aasheduled_books" , id = "_id")*/
+@Table(name = "aasheduled_books" , id = "_id")
 public class AAScheduled_Books extends Model {
 
 
